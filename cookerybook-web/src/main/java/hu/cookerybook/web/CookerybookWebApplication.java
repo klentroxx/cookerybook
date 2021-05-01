36bec139601e78acae63265b42ff1b3fba01d8a2
fityo <fityo.andris@gmail.com>
2021-05-01 19:16:24 +0200

    Spring Initialized

---
+++ cookerybook-web/src/main/java/hu/cookerybook/web/CookerybookWebApplication.java
@@ -0,0 +1,13 @@
+package hu.cookerybook.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookerybookWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookerybookWebApplication.class, args);
+    }
+
+}
